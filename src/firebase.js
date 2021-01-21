f7d2e69fed66fb96d26a76f27ec823eec0111eed
tomas <tomasberhe1999@gmail.com>
2021-01-21 11:02:41 +0100

    bug fixed

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,6 @@
 import firebase from 'firebase'
 const firebaseConfig = {
-    apiKey: "AIzaSyAWHPNvtrWmMniPYRPu3mX2ohmCJW4G-vU",
-    authDomain: "telegramclone-ac9a2.firebaseapp.com",
-    databaseURL: "https://telegramclone-ac9a2.firebaseio.com",
-    projectId: "telegramclone-ac9a2",
-    storageBucket: "telegramclone-ac9a2.appspot.com",
-    messagingSenderId: "771549601430",
-    appId: "1:771549601430:web:2c4f4595d068a62fdfecb7"
+    //add your own config
   };
   const firebaeApp = firebase.initializeApp(firebaseConfig)
   const db = firebaeApp.firestore()
